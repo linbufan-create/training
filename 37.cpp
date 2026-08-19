3a2693b92f205dfcc74d080efa76b10efaf466d5
林布帆 <3627665738@qq.com>
2026-08-19 17:18:07 +0800

    37.cppAC

--- 37.cpp
+++ 37.cpp
@@ -33,126 +33,103 @@ l。
 0。
 */
 #include <bits/stdc++.h>
-#define  int long long
+#define int long long
 using namespace std;
 int n, k;
+vector<int> prime;
 
 class __prime {
 public:
-	vector<pair<int, int>> prime;
-
-	__prime(vector<pair<int, int>> dat) {
-		prime = dat;
-	}
+	int prime[10];
 
 	__prime(int x) {
-		vector<pair<int, int>> data;
-		for (int i = 2; i * i <= x; i++) {
-			if (x % i == 0) {
+		for (int i = 0; i < 10; i++) {
+			prime[i]=0;
+		}
+		for (int i = 0; i < ::prime.size(); i++) {
+			if (x % ::prime[i] == 0) {
 				int cnt = 0;
-				while (x % i == 0) {
+				while (x % ::prime[i]==0) {
 					cnt++;
-					x /= i;
+					x /= ::prime[i];
 				}
-				data.push_back({i, cnt});
+				prime[i + 1] = cnt;
 			}
 		}
-		if (x > 1) {
-			data.push_back({x, 1});
-		}
-		prime = data;
 	}
 
-	__prime operator-(const __prime& other) const {
-		vector<pair<int, int>> th = this->prime, oth = other.prime;
-		for (auto& p1 : th) {
-			for (auto p2 : oth) {
-				if (p1.first == p2.first) {
-					p1.second -= p2.second;
-				}
-			}
-		}
-		return th;
-	}
-
-	__prime operator+(const __prime& other) {
-		vector<pair<int, int>> th = this->prime, oth = other.prime;
-		for (auto& p1 : th) {
-			for (auto p2 : oth) {
-				if (p1.first == p2.first) {
-					p1.second += p2.second;
-				}
-			}
-		}
-		return th;
-	}
-
-	bool operator<(const __prime& other) const {
-		bool big = 0;
-		for (auto p : other.prime) {
-			bool can = 0;
-			for (auto p2 : this->prime) {
-				if (p.first == p2.first) {
-					can = 1;
-					if (p.second > p2.second) {
-						big = 1;
-					}
-				}
-			}
-			if (!can) {
-				return 0;
-			}
-		}
-		return big;
-	}
-
-	bool operator>=(const __prime& other) const {
-		bool big = 0;
-		for (auto p : other.prime) {
-			bool can = 0;
-			for (auto p2 : this->prime) {
-				if (p.first == p2.first) {
-					can = 1;
-					if (p.second > p2.second) {
-						big = 1;
-					}
-				}
-			}
-			if (!can) {
+	bool operator<(const __prime& other) {
+		for (int i = 1; i <= ::prime.size(); i++) {
+			if (this->prime[i] < other.prime[i]) {
 				return 1;
 			}
 		}
-		return !big;
+		return 0;
 	}
 
-	void operator+=(const __prime& other) {
-		*this = *this + other;
+	bool operator>=(const __prime& other) {
+		return !(*this < other);
 	}
 
-	void operator-=(const __prime& other) {
-		*this = *this - other;
+	__prime operator+=(const __prime& other) {
+		for (int i = 1; i <= ::prime.size(); i++) {
+			this->prime[i] += other.prime[i];
+		}
+		return *this;
+	}
+
+	__prime operator-=(const __prime& other) {
+		for (int i = 1; i <= ::prime.size(); i++) {
+			this->prime[i] -= other.prime[i];
+		}
+		return *this;
+	}
+
+	__prime operator+(const __prime& other) {
+		__prime tmp = *this;
+		tmp += other;
+		return tmp;
+	}
+
+	__prime operator-(const __prime& other) {
+		__prime tmp = *this;
+		tmp -= other;
+		return tmp;
 	}
 };
 
 int ans;
+int k2;
 
 signed main() {
 	freopen("num.in", "r", stdin);
-	freopen("num.out","w",stdout);
+	freopen("num.out", "w", stdout);
 	cin >> n >> k;
+	k2 = k;
 	if (k == 1) {
 		cout << n * (n + 1) / 2 << endl;
 		return 0;
 	}
-	__prime p = __prime(k);
-	__prime sum = (k);
-	for (auto& p : sum.prime) {
-		p.second = 0;
+	for (int i = 2; i * i <= k; i++) {
+		if (k % i == 0) {
+			prime.push_back(i);
+			while (k % i == 0) {
+				k /= i;
+			}
+		}
+	}
+	if (k > 1) {
+		prime.push_back(k);
+	}
+	__prime p = __prime(k2);
+	__prime sum = p;
+	for (int i = 1; i <= prime.size(); i++) {
+		sum.prime[i] = 0;
 	}
 	for (int l = 1, r = 1; r <= n; r++) {
 		sum += __prime(r);
-		while (sum - __prime(l) >= p) {
-			sum -= __prime(l);
+		while (l < r && sum - __prime(l) >= p) {
+			sum-=__prime(l);
 			l++;
 		}
 		if (sum >= p) {
